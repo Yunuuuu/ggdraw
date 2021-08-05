477894a2a0eacad67e3259c24c2ea7eefe7b303b
yun <yunyunpp96@outlook.com>
2021-08-05 23:56:52 +0800

    complete package Rd

---
+++ R/ggdraw.R
@@ -0,0 +1,16 @@
+#' ggdraw: Draw with grob or function in ggplot2
+#'
+#' @description Draw with grob or function in ggplot2. This package is inspired
+#'   by [gggrid](https://github.com/pmur002/gggrid) but adjusted with a lot of
+#'   personal convention. See [gggrid: it's
+#'   g-g-great!](https://www.stat.auckland.ac.nz/~paul/Reports/gggrid/gggrid.html)
+#'
+#' @author Yun \email{yunyunpp96@@outlook.com}
+#' @seealso Useful links:
+#' * \url{https://ggplot2.tidyverse.org}
+#' * \url{https://github.com/tidyverse/ggplot2}
+#' * \url{https://github.com/Yunuuuu/ggdraw}
+#' * [gggrid: it's g-g-great!](https://www.stat.auckland.ac.nz/~paul/Reports/gggrid/gggrid.html)
+#' @docType package
+#' @name ggdraw
+NULL
