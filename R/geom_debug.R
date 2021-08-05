3bac6477c9ee4731d207ad83f0646fc2b594b607
yun <yunyunpp96@outlook.com>
2021-08-05 20:49:50 +0800

    geom_draw and geom_debug

---
+++ R/geom_debug.R
@@ -0,0 +1,78 @@
+#' Debug ggplot2
+#'
+#' Debug ggplot2 by operate data with a function.
+#'
+#' @param debug a function which conducted with argument \code{coords}. \cr \cr
+#'   coords is value that have already been transformed to the plot scales.
+#'   Default: `head`
+#' @param type \code{group} or \code{panel}, group which draws geom with
+#'   \code{draw_group} draws collective geoms that display multiple observations
+#'   with one geometric object and \code{panel} which draws geom with
+#'   \code{draw_panel} draws individual geoms that display a distinct graphical
+#'   object for each observation (row). Default: `group`
+#' @inheritParams ggplot2::layer
+#' @param ... Other arguments passed on to [ggplot2::layer()]. These are often
+#'   aesthetics, used to set an aesthetic to a fixed value, like colour = "red"
+#'   or size = 3. They may also be parameters to the paired geom/stat.
+#' @export
+geom_debug <- function(debug = "head", type = NULL,
+                       mapping = NULL, data = NULL, stat = "identity",
+                       position = "identity", ..., inherit.aes = TRUE) {
+
+  if (is.null(type)) type <- "group" else {
+    type <- tolower(as.character(type))
+    type <- match.arg(type, c("panel", "group"))
+  }
+
+  test_debug <- rlang::is_function(debug) || rlang::is_scalar_character(debug) || rlang::is_symbol(debug)
+
+  if (!test_debug) {
+    stop("Invalid 'debug' argument; debug must be a function to call ",
+         "(can be a string, symbol, call, or a function).",
+         call. = FALSE)
+  }
+
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = switch (type,
+                   panel = GeomDebugPanel,
+                   group = GeomDebugGroup
+    ),
+    position = position,
+    show.legend = FALSE,
+    inherit.aes = inherit.aes,
+    params = list(debug = debug, ...)
+  )
+
+}
+
+draw_debug <- function(data, panel_params, coord, debug) {
+
+  coords <- coord$transform(data, panel_params)
+  debug(coords)
+
+}
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomDebugPanel <- ggplot2::ggproto(
+  "GeomDebugPanel", ggplot2::Geom,
+  ## No required_aes
+  ## No default_aes
+  ## No draw_key
+  draw_panel = draw_debug)
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomDebugGroup <- ggplot2::ggproto(
+  "GeomDebugGroup", ggplot2::Geom,
+  ## No required_aes
+  ## No default_aes
+  ## No draw_key
+  draw_group = draw_debug)
---
+++ R/geom_grid.R
@@ -0,0 +1,107 @@
+#' Layer with Grid or Function
+#'
+#' Draw ggplot2 layer with a grod or function.
+#'
+#' @param draw Either a [grob][grid::grob] object or a function which accepts
+#'   two arguments (\code{data} and \code{coords}) and returns a
+#'   [grob][grid::grob]. \cr \cr \code{data} contains value from parameter
+#'   \code{data}, coords is value that have already been transformed to the plot
+#'   scales.
+#' @param type \code{group} or \code{panel}, group which draws geom with
+#'   \code{draw_group} draws collective geoms that display multiple observations
+#'   with one geometric object and \code{panel} which draws geom with
+#'   \code{draw_panel} draws individual geoms that display a distinct graphical
+#'   object for each observation (row). Default: `group`
+#' @inheritParams ggplot2::layer
+#' @param ... Other arguments passed on to [ggplot2::layer()]. These are often
+#'   aesthetics, used to set an aesthetic to a fixed value, like colour = "red"
+#'   or size = 3. They may also be parameters to the paired geom/stat.
+#' @return a ggplot2 layer object
+#' @examples
+#' gggrid_text <- grid::textGrob(
+#'   "gggrid",
+#'   x = c(0, 0, 0.5, 1, 1),
+#'   y = c(0, 1, 0.5, 0, 1),
+#'   hjust = c(0, 0, 0.5, 1, 1),
+#'   vjust = c(0, 1, 0.5, 0, 1)
+#' )
+#' ggplot2::ggplot(data.frame(x = 1, y = 2))+
+#'   geom_draw(gggrid_text)
+#' @export
+geom_draw <- function(draw = grid::nullGrob(), type = NULL,
+                      mapping = NULL, data = NULL, stat = "identity",
+                      position = "identity", ..., inherit.aes = TRUE) {
+
+  if (is.null(type)) type <- "group" else {
+    type <- tolower(as.character(type))
+    type <- match.arg(type, c("panel", "group"))
+  }
+
+  test_draw <- grid::is.grob(draw) || rlang::is_function(draw) || rlang::is_scalar_character(draw) || rlang::is_symbol(draw)
+
+  if (!test_draw) {
+    stop("Invalid 'draw' argument; draw must be a grob ",
+         "or a function to call (can be a string, ",
+         "symbol, call, or a function).",
+         call. = FALSE)
+  }
+
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = switch (type,
+                   panel = GeomDrawPanel,
+                   group = GeomDrawGroup
+    ),
+    position = position,
+    show.legend = FALSE,
+    inherit.aes = inherit.aes,
+    params = list(draw = draw, ...)
+  )
+
+}
+
+draw_fn <- function(data, panel_params, coord, draw) {
+
+  if (grid::is.grob(draw)) {
+
+    draw
+
+  } else {
+
+    coords <- coord$transform(data, panel_params)
+    rlang::exec(draw, data = data, coords = coords)
+
+  }
+}
+
+#' @inherit ggplot2::Geom title seealso
+#' @inheritSection  ggplot2::Geom Geoms
+#' @inheritSection  ggplot2::Coord Coordinate systems
+#' @import ggplot2
+#' @import rlang
+#' @name ggplot2-ggproto
+NULL
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomDrawPanel <- ggplot2::ggproto(
+  "GeomDrawPanel", ggplot2::Geom,
+  ## No required_aes
+  ## No default_aes
+  ## No draw_key
+  draw_panel = draw_fn)
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+GeomDrawGroup <- ggplot2::ggproto(
+  "GeomDrawGroup", ggplot2::Geom,
+  ## No required_aes
+  ## No default_aes
+  ## No draw_key
+  draw_group = draw_fn)
